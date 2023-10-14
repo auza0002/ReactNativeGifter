36f4655455b897881213292b33b03419bf06bf5e
Diego Auza <auza0002@algonquinlive.com>
2023-10-14 14:33:12 -0400

    navigator

---
+++ screens/PeopleScreen.js
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+const PeopleScreen = () => {
+  return (
+    <View>
+      <Text>People Screen</Text>
+    </View>
+  );
+};
+export default PeopleScreen;
