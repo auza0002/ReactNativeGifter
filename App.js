0c6f670efd834b5dfc44c59f571af3ff66a63615
Diego Auza <auza0002@algonquinlive.com>
2023-10-14 14:32:56 -0400

    navigator

--- App.js
+++ App.js
@@ -2,14 +2,20 @@ import { StatusBar } from "expo-status-bar";
 import { useState, useEffec } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { MyDataProvider } from "./context/AsyncStorage";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import PeopleScreen from "./screens/PeopleScreen";
 
 export default function App() {
-  const [peopleDataList, setPeopleDataList] = useState([]);
   return (
     <MyDataProvider>
-      <View>
-        <Text>Hello asdfsdfsafs</Text>
-      </View>
+      <SafeAreaProvider>
+        <SafeAreaView
+          style={{ flex: 1 }}
+          edges={["right", "bottom", "left", "top"]}
+        >
+          <PeopleScreen />
+        </SafeAreaView>
+      </SafeAreaProvider>
     </MyDataProvider>
   );
 }
